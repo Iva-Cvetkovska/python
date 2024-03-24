28217b2a9f570c137b88e9401b0dd5a851c5daa4
Iva-Cvetkovska <ivacvetkovska.freelance@gmail.com>
2024-03-24 23:59:35 +0100

    Translate to english

--- Search/informed_search/climber.py
+++ Search/informed_search/climber.py
@@ -6,13 +6,13 @@ from searching_framework.informed_search import astar_search
 
 def move_house(x, direction, board_size):
     if x == board_size - 1:
-        return x - 1, 'levo'
+        return x - 1, 'left'
     if x == 0:
-        return x + 1, 'desno'
-    if direction == 'desno':
-        return x + 1, 'desno'
-    if direction == 'levo':
-        return x - 1, 'levo'
+        return x + 1, 'right'
+    if direction == 'right':
+        return x + 1, 'right'
+    if direction == 'left':
+        return x - 1, 'left'
 
 
 class Climber(Problem):
@@ -32,38 +32,38 @@ class Climber(Problem):
 
         new_house_x, new_house_dir = move_house(house_x, house_dir, self.N)
 
-        # Stoj
-        successors['Stoj'] = ((human_x, human_y), (new_house_x, house_y), new_house_dir)
+        # Stop
+        successors['Stop'] = ((human_x, human_y), (new_house_x, house_y), new_house_dir)
 
-        # Gore 1
+        # Up 1
         new_y = human_y + 1
         if self.is_valid(human_x, new_y) or (human_x, new_y) == (new_house_x, house_y):
-            successors['Gore 1'] = ((human_x, new_y), (new_house_x, house_y), new_house_dir)
+            successors['Up 1'] = ((human_x, new_y), (new_house_x, house_y), new_house_dir)
 
-        # Gore 2
+        # Up 2
         new_y = human_y + 2
         if self.is_valid(human_x, new_y) or (human_x, new_y) == (new_house_x, house_y):
-            successors['Gore 2'] = ((human_x, new_y), (new_house_x, house_y), new_house_dir)
+            successors['Up 2'] = ((human_x, new_y), (new_house_x, house_y), new_house_dir)
 
-        # Gore-desno 1
+        # Up-right 1
         new_x, new_y = human_x + 1, human_y + 1
         if self.is_valid(new_x, new_y) or (new_x, new_y) == (new_house_x, house_y):
-            successors['Gore-desno 1'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
+            successors['Up-right 1'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
 
-        # Gore-desno 2
+        # Up-right 2
         new_x, new_y = human_x + 2, human_y + 2
         if self.is_valid(new_x, new_y) or (new_x, new_y) == (new_house_x, house_y):
-            successors['Gore-desno 2'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
+            successors['Up-right 2'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
 
-        # Gore-levo 1
+        # Up-left 1
         new_x, new_y = human_x - 1, human_y + 1
         if self.is_valid(new_x, new_y) or (new_x, new_y) == (new_house_x, house_y):
-            successors['Gore-levo 1'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
+            successors['Up-left 1'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
 
-        # Gore-levo 2
+        # Up-left 2
         new_x, new_y = human_x - 2, human_y + 2
         if self.is_valid(new_x, new_y) or (new_x, new_y) == (new_house_x, house_y):
-            successors['Gore-levo 2'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
+            successors['Up-left 2'] = ((new_x, new_y), (new_house_x, house_y), new_house_dir)
 
         return successors
 
